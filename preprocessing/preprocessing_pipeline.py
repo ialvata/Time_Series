b6944c825d9415082f1814e732e68a7f2e8e3a12
I T <ivo.a.v.tavares@gmail.com>
2023-07-14 16:58:21 +0100

    Added some initial preprocessing transformations.

--- preprocessing/preprocessing_pipeline.py
+++ preprocessing/preprocessing_pipeline.py
@@ -3,11 +3,15 @@ from  abc import ABC, abstractmethod
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
+from scipy import stats
 
-class PreprocessingPipeline(ABC):
-    def __init__(self, path_to_data: Path | None = None):
+class PreprocessPipeline(ABC):
+    def __init__(self, path_to_data: Path, train_prop: float):
         if path_to_data:
             self.dataframe :pd.DataFrame = self.fetch_data(path=path_to_data)
+        test_rows = 1 - int(train_prop * self.dataframe.shape[0])
+        self.test:pd.DataFrame = self.dataframe.iloc[-test_rows:]
+        self.dataframe = self.dataframe.iloc[:test_rows]
 
     def add_lags(self, df:pd.DataFrame, n_lags:int|list[int], drop_cols:list)-> pd.DataFrame:
         """
@@ -36,3 +40,44 @@ class PreprocessingPipeline(ABC):
         # read raw data
         dataframe = pd.read_csv(path)
         return dataframe
+
+
+class Stationarize(PreprocessPipeline):
+    """
+    This class has several transformations that can help us to stationarize
+    the original data.
+    """
+    def apply_diff(self,columns: list[str], periods:int) -> pd.DataFrame:
+        """ 
+        This method will return a smaller dataframe than the one entered, since the initial
+        `periods` rows will be NaN, and dropped.       
+        """
+        return self.dataframe[columns].diff(periods=periods).dropna()
+    
+    def invert_diff(self,df_diff:pd.DataFrame, periods:int):
+        columns_indices = self.dataframe.columns.get_indexer(df_diff.columns)
+        initial_rows = self.dataframe.iloc[:periods,columns_indices]
+        concat_df:pd.DataFrame = pd.concat([initial_rows, df_diff])
+        return concat_df.cumsum()
+        
+    
+    def apply_box_cox(self, columns: list[str], 
+                      lambda:float | None = None,
+                      alpha: float | None = None) -> tuple[pd.DataFrame,dict]:
+        """
+        lambda : {None, scalar}, optional
+            If lmbda is not None, do the transformation for that value. 
+            If lmbda is None, find the lambda that maximizes the log-likelihood 
+                function and return it as the second output argument.
+        """
+        dict_lambda={}
+        df_box_cox=pd.DataFrame()
+        for column in columns:
+            box_cox_data, lambda_par = stats.boxcox(self.dataframe[column].values,
+                                                    lmbda = lambda)
+            df_box_cox[column]= box_cox_data
+            dict_lambda[column]=lambda_par
+        return df_box_cox, dict_lambda
+
+    def invert_box_cox(self):
+        stats.inv_boxcox(data_series, lambda)
