2c82818d833f723d0d1739faa6689cbb1ec81211
I T <ivo.a.v.tavares@gmail.com>
2023-07-26 21:53:06 +0100

    improved visualize loss method

--- models/lstm.py
+++ models/lstm.py
@@ -1,10 +1,11 @@
+import matplotlib.pyplot as plt
+
 import keras
 from keras import Model, Sequential
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 from keras.losses import MeanSquaredError
 from keras.metrics import MeanAbsoluteError
-
 from keras.layers import Dense, Conv1D, LSTM, Lambda, Reshape, RNN, LSTMCell
 
 
@@ -16,7 +17,7 @@ class LSTMKeras:
                 Dense(units=1)
             ]
         )
-        self.history = None
+        self.history: keras.callbacks.History | None = None
     def compile(self):
         
     
@@ -27,21 +28,24 @@ class LSTMKeras:
         early_stopping = EarlyStopping(monitor='val_loss',
                                    patience=patience,
                                    mode='min')
-        self.history: keras.callbacks.History = self.model.fit(
+        self.history = self.model.fit(
             train_data,#window.train,
             epochs=max_epochs,
             validation_data=val_data,#window.val,
             callbacks=[early_stopping])
         
     def visualize_loss(self, title):
-        loss = self.history.history["loss"]
-        val_loss = self.history.history["val_loss"]
-        epochs = range(len(loss))
-        plt.figure()
-        plt.plot(epochs, loss, "b", label="Training loss")
-        plt.plot(epochs, val_loss, "r", label="Validation loss")
-        plt.title(title)
-        plt.xlabel("Epochs")
-        plt.ylabel("Loss")
-        plt.legend()
-        plt.show()
+        if self.history is not None:
+            loss = self.history.history["loss"]
+            val_loss = self.history.history["val_loss"]
+            epochs = range(len(loss))
+            plt.figure()
+            plt.plot(epochs, loss, "b", label="Training loss")
+            plt.plot(epochs, val_loss, "r", label="Validation loss")
+            plt.title(title)
+            plt.xlabel("Epochs")
+            plt.ylabel("Loss")
+            plt.legend()
+            plt.show()
+        else:
+            raise Exception("Unfitted model has no History object!")
