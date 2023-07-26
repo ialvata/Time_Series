5b0410ee1e4175c650455f075e135a59b00adaf2
I T <ivo.a.v.tavares@gmail.com>
2023-07-26 21:44:22 +0100

    created sketch for lstm model

---
+++ models/lstm.py
@@ -0,0 +1,47 @@
+import keras
+from keras import Model, Sequential
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from keras.losses import MeanSquaredError
+from keras.metrics import MeanAbsoluteError
+
+from keras.layers import Dense, Conv1D, LSTM, Lambda, Reshape, RNN, LSTMCell
+
+
+class LSTMKeras:
+    def __init__(self) -> None:
+        self.model = Sequential(
+            [
+                LSTM(32, return_sequences=True),
+                Dense(units=1)
+            ]
+        )
+        self.history = None
+    def compile(self):
+        
+    
+        self.model.compile(loss=MeanSquaredError(),
+                  optimizer=Adam(),
+                  metrics=[MeanAbsoluteError()])
+    def fit(self, train_data, val_data, max_epochs: int, patience: int = 0):
+        early_stopping = EarlyStopping(monitor='val_loss',
+                                   patience=patience,
+                                   mode='min')
+        self.history: keras.callbacks.History = self.model.fit(
+            train_data,#window.train,
+            epochs=max_epochs,
+            validation_data=val_data,#window.val,
+            callbacks=[early_stopping])
+        
+    def visualize_loss(self, title):
+        loss = self.history.history["loss"]
+        val_loss = self.history.history["val_loss"]
+        epochs = range(len(loss))
+        plt.figure()
+        plt.plot(epochs, loss, "b", label="Training loss")
+        plt.plot(epochs, val_loss, "r", label="Validation loss")
+        plt.title(title)
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+        plt.legend()
+        plt.show()
