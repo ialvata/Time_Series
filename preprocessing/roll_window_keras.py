cfc6ac8ba609cd0ceaded8552d5d6274d500b214
I T <ivo.a.v.tavares@gmail.com>
2023-07-27 16:08:13 +0100

    updated keras roll window.

--- preprocessing/roll_window_keras.py
+++ preprocessing/roll_window_keras.py
@@ -15,15 +15,28 @@ from keras.preprocessing import timeseries_dataset_from_array
 class KerasWindow(RollWindow):
     def __init__(self, 
                  data_split: Split,
-                 sequence_length: int,
+                 input_length: int,
+                 label_length: int,
                  sequence_stride: int,
                  label_columns: list[str] | None = None,
                  shuffle:bool = False, batch_size: int = 128
                  ):
         """
-        See
+        This will serve as x argument to the `fit` method of the Keras Model API.
+        The fit method, in the x argument, will take a `tf.data.Dataset` like object.
+        In this case, it should be a **tuple** of either (inputs, targets) or 
+        (inputs, targets, sample_weights).
+            See `https://keras.io/api/models/model_training_apis/`
+        
+        There are two main ways to construct the Window:
+            - Either we use the Split object, which has the train/val/test dataframes, and from it 
+            we construct a `tf.data.Dataset`, and then we filter for inputs and labels; 
+            - Or we use the Split object, filter for inputs and labels, and only then do we
+            construct `tf.data.Dataset` for each inputs df and labels df.
+        
+        The second method may be too ineficient memory wise, while the first may be too slow, for 
+        a great amount of data.
         """
-
         self.data_split = data_split
         self.label_columns = label_columns
         if label_columns is not None:
@@ -31,20 +44,19 @@ class KerasWindow(RollWindow):
         self.column_indices = {
             name: i for i, name in enumerate(self.data_split.train_df.columns)
         }
-        # these two parameters should be created in the data_split instead.
-        self.sequence_length = sequence_length
+        self.input_length = input_length
+        self.sequence_length = input_length + label_length
         self.sequence_stride = sequence_stride
         self.shuffle = shuffle
         self.batch_size = batch_size
 
-    @property
-    def keras_train_set(self) -> tf.data.Dataset:
+    def create_keras_dataset(self, dataframe:pd.DataFrame) -> tf.data.Dataset:
         """
         Example
         -------
         Consider indices [0, 1, ... 98]. With sequence_length=10, sampling_rate=2, 
-        sequence_stride=3, batch_size = 2, shuffle=False, the dataset will yield batches of sequences composed 
-        of the following indices:
+        sequence_stride=3, batch_size = 2, shuffle=False, the dataset will yield batches of 
+        sequences composed of the following indices:
             First batch:
                 Inputs:
                     First sequence:  [0  2  4  6  8 10 12 14 16 18]
@@ -67,14 +79,41 @@ class KerasWindow(RollWindow):
         In this case the last 2 data points are discarded since no full sequence can be 
         generated to include them (the next sequence would have started at index 81, and 
         thus its last step would have gone over 98).
+
+        Hence, `tf.data.Dataset` is a generator of tensors of 
+        dim(batch_size, sequence_length, num_features), where sequence_length will be the 
+        window size (input length plus label length).
         """
-        return timeseries_dataset_from_array(
-            data = self.data_split.train_input,
-            targets = self.data_split.train_labels,
+
+        ds:tf.data.Dataset = timeseries_dataset_from_array(
+            data = dataframe.to_numpy(), # must be a numpy array
+            targets = None, # if we used this, we would be limited to single-step forecasts
             sequence_length = self.sequence_length,
             sequence_stride = self.sequence_stride,
             shuffle = self.shuffle,
             batch_size = self.batch_size
         )
-
+        ds.map(self.filter,
+            # num_parallel_calls=tf.data.AUTOTUNE
+        )
+        return ds
     
+    def filter(self, tensor:tf.Tensor):
+        """
+        Each tensor is of shape dim(batch_size, sequence_length, num_features), where 
+        sequence_length will be the window size (input length plus label length).
+        """
+        inputs = tensor[:,:self.input_length,:]
+        labels = tensor[:,self.input_length:,:]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [
+                    labels[:,:,self.column_indices[name]]
+                    for name in self.label_columns
+                ]
+            )
+        return inputs, labels
+    
+    @property
+    def train_data(self):
+         return self.create_keras_dataset(self.data_split.train_df)
---
+++ tests/LSTM_experiment.py
@@ -0,0 +1,64 @@
+from preprocessing.preprocess_base import Preprocess
+from preprocessing.preprocess_input_base import PreprocessInput
+from pathlib import Path
+from pandas import DatetimeIndex,to_datetime
+from preprocessing.set_splitting import Split
+from preprocessing.roll_window_keras import KerasWindow
+
+
+
+#####################      Downloading first time, only       #################################
+
+# from zipfile import ZipFile
+# import os
+# import keras
+
+# uri = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip"
+# zip_path = keras.utils.get_file(origin=uri, fname="jena_climate_2009_2016.csv.zip")
+# zip_file = ZipFile(zip_path)
+# zip_file.extractall()
+# csv_path = "jena_climate_2009_2016.csv"
+
+path_to_data=Path(
+    "/home/ivo/Programming_Personal_Projects/Time_Series/datasets/csv/"
+)/"temperature_jena_climate_2009_2016.csv"
+
+input = PreprocessInput(path_to_data)
+
+
+class AirTemperature(Preprocess):
+    def __init__(self,input:PreprocessInput):
+        Preprocess.__init__(self,input=input)
+        self.cleaned = False
+
+    def clean_dataframe(self):
+        if not self.cleaned:
+            self.dataframe.index = DatetimeIndex(
+                # use of to_datetime makes the conversion must faster
+                to_datetime(
+                    self.dataframe["Date Time"],
+                    format="%d.%m.%Y %H:%M:%S"
+                ))
+            self.dataframe.drop(["Date Time"],axis=1, inplace=True)
+            self.cleaned = True
+        else:
+            print("Dataframe has already been cleaned")
+
+air_temp = AirTemperature(input)
+air_temp.clean_dataframe()
+# air_temp.plot_heatmap()
+
+data_split = Split(air_temp.dataframe, label_columns=["T (degC)"])
+data_split.test_labels
+data_split.test_labels
+data_split.val_labels
+assert (
+    len(data_split.train_df) + len(data_split.test_df)+len(data_split.val_df) 
+    == 
+    air_temp.dataframe.shape[0]
+)
+
+
+window = KerasWindow(data_split, 10, 1,1, label_columns=["T (degC)"])
+[element for element in window.train_data]
+print("ola")
