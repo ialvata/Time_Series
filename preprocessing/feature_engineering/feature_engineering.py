c95357d9f6488262c3ce9652a072d171018e4ad0
I T <ivo.a.v.tavares@gmail.com>
2023-09-06 20:01:53 +0100

    Added fourier features. Still testing.

--- preprocessing/feature_engineering/feature_engineering.py
+++ preprocessing/feature_engineering/feature_engineering.py
@@ -18,6 +18,20 @@ class TrigSeason(IntEnum):
 class BaseScaler:
     pass
 
+class SeasonLength(IntEnum):
+    """
+    Length of the seasonality cycle.
+    For more information, read the docstring in `_calculate_fourier_terms` method, below.
+    """
+    DAY_OF_YEAR = 366 # DateTimeIndex.day_of_year
+    WEEK_OF_YEAR = 52 # DateTimeIndex.isocalendar().week
+    MONTH_OF_YEAR = 12 # DateTimeIndex.month
+    DAY_OF_WEEK = 7 # DateTimeIndex.day_of_week
+    WEEK_OF_MONTH = 5
+    DAY_OF_MONTH = 31 # DateTimeIndex.day
+
+
+
 class FeatureEngineering(ABC):
     def __init__(self, dataframe:pd.DataFrame, 
                  scaler:MinMaxScaler | StandardScaler | MaxAbsScaler | None = None):
@@ -105,7 +119,139 @@ class FeatureEngineering(ABC):
         else:
             full_list_df = [self._dataframe] + appended_lags
             self._dataframe = pd.concat(full_list_df, axis=1)
+    
+    def _calculate_fourier_terms(self,
+        seasonal_cycle: np.ndarray, cycle_len: int, n_fourier_terms: int
+    ):
+        """
+        Calculates Fourier Terms which will be used as features.
 
+        Parameters
+        ----------
+        `seasonal_cycle`:
+            For a day-of-week cycle, we expect an iterable: 
+                [1, 2,..., 7, 1, 2,...]
+                Here `cycle_len` is 7.
+            For a day-of-month cycle, we expect: 
+                [1, 2, ..., 31, 1,...]
+                Here `cycle_len` is 31. 
+                Note that not all months will have the same length.
+            For a day-of-year cycle, we expect: 
+                [1,...,366,1,...]
+                Here `cycle_len` is 366.
+                Note that not all years will have the same length.
+            For month-of-year cycle, we expect:
+                [1, 2,..., 12, 1,...]
+                Here `cycle_len` is 12.
+        """
+        sin_array = np.empty((len(seasonal_cycle), n_fourier_terms), dtype="float64")
+        cos_array = np.empty((len(seasonal_cycle), n_fourier_terms), dtype="float64")
+        for n in range(1, n_fourier_terms + 1):
+            # Fourier(x) =  sum_{i=1}^{N} A_n * cos(2*pi / P * n * x) + B_n sin(2*pi/P*n*x)
+            # seasonal_cycle array gives us the x values.
+            sin_array[:, n - 1] = np.sin((2 * np.pi * seasonal_cycle * n) / cycle_len)
+            cos_array[:, n - 1] = np.cos((2 * np.pi * seasonal_cycle * n) / cycle_len)
+        return np.hstack([sin_array, cos_array])
+
+    def add_fourier_features(self,
+                             seasonal_lengths:list[SeasonLength],
+                             columns:list[str] | None = None,
+                             num_terms:list[int] | None = None,
+                             return_output: bool = False)-> pd.DataFrame | None:
+        """
+        This function will create Fourier features to represent explicitely time as a 
+        regressor.
+
+        Parameters
+        ----------
+        `seasonal_lengths:list[SeasonLength]`
+
+        `columns:list[str]| None = None`
+            List of columns names for which we want to build the Fourier terms
+            If `None`, then we automatically create the Fourier terms from the DataFrame index,
+            when it's a DateTimeIndex.
+
+        `terms:list[int] | None = None`
+            List of numbers of Fourier terms for each element of seasonal length.
+
+        Note
+        ----
+        seasonal_lengths, n_terms, and columns should match on the element order.
+
+        TODO:
+            Implement case for SeasonLength.WEEK_OF_MONTH.
+        """
+        if num_terms is None:
+                # Unless otherwise stated, we'll use 5 as the number of Fourier Terms
+                # for all new fourier features.
+                num_terms = [5]*len(seasonal_lengths)
+        if not isinstance(self._dataframe.index,pd.DatetimeIndex):
+                raise Exception(
+                    "Please convert (internal) DataFrame Index to a DatetimeIndex!"
+                )
+        fourier_columns = []
+        fourier_names = []
+        if columns is None:
+            for season_len,num_term in zip(seasonal_lengths,num_terms):
+                sin_columns = [
+                    f"sin_term_{num}_{season_len.name}" for num in range(1,num_term+1)
+                ]
+                cos_columns = [
+                    f"cos_term_{num}_{season_len.name}" for num in range(1,num_term+1)
+                ]
+                match season_len:
+                    case SeasonLength.DAY_OF_YEAR:
+                        fourier_array = self._calculate_fourier_terms(
+                            seasonal_cycle=self._dataframe.index.day_of_year.values,
+                            cycle_len=SeasonLength.DAY_OF_YEAR,
+                            n_fourier_terms=num_term
+                        )
+                    case SeasonLength.WEEK_OF_YEAR: 
+                        fourier_array = self._calculate_fourier_terms(
+                            seasonal_cycle = np.array(
+                                self._dataframe.index.isocalendar().week.values
+                                ),
+                            cycle_len=SeasonLength.WEEK_OF_YEAR,
+                            n_fourier_terms=num_term
+                        )
+                    case SeasonLength.MONTH_OF_YEAR: 
+                        fourier_array = self._calculate_fourier_terms(
+                            seasonal_cycle = np.array(
+                                self._dataframe.index.month.values
+                                ),
+                            cycle_len=SeasonLength.MONTH_OF_YEAR,
+                            n_fourier_terms=num_term
+                        )
+                    case SeasonLength.DAY_OF_MONTH: 
+                        fourier_array = self._calculate_fourier_terms(
+                            seasonal_cycle = np.array(
+                                self._dataframe.index.day.values
+                                ),
+                            cycle_len=SeasonLength.DAY_OF_MONTH,
+                            n_fourier_terms=num_term
+                        )
+                    # TODO WEEK_OF_MONTH
+                    case SeasonLength.WEEK_OF_MONTH:
+                        raise Exception(
+                            "The option WEEK_OF_MONTH has not been implemented yet!"
+                            "Please choose another option."
+                        )
+                    case _:
+                        raise Exception(
+                            "Invalid SeasonLength option! Please choose a valid one."
+                        )
+                fourier_columns.append(np.hstack([fourier_array]))
+                fourier_names.append(np.hstack([sin_columns,cos_columns]))
+            pd.DataFrame(np.hstack(fourier_columns),
+                        columns = np.hstack(fourier_names))
+                
+        else:
+            for column,season_len,num_term in zip(columns,seasonal_lengths,num_terms):
+                self._calculate_fourier_terms(
+                            seasonal_cycle=np.array(self._dataframe[column]),
+                            cycle_len=season_len,
+                            n_fourier_terms=num_term
+                        )
 
     def scale_features(self)-> None:
         """
