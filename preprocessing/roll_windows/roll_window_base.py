eaaa686743d450c0eaf2bcdf170c7936dc9e27cd
I T <ivo.a.v.tavares@gmail.com>
2023-09-07 17:56:38 +0100

    Classical Window returns TrainSet and TestSet instances... Comment them out. KISS

--- preprocessing/roll_windows/roll_window_base.py
+++ preprocessing/roll_windows/roll_window_base.py
@@ -16,20 +16,59 @@ class RollWindow(ABC):
                  label_columns: list[str] | None = None):
         pass
 
-    # def create_folds(self) -> Generator[tuple[pd.DataFrame, pd.DataFrame],None,None]:
-        
-    #     for index in range(self.train_length,self.dataframe.shape[0], self.window):
-    #         yield (self.dataframe.iloc[:index], self.dataframe.iloc[[index]])
+class TrainSet:
+    def __init__(self, 
+                 dataframe:pd.DataFrame,labels_names:list[str], 
+                 feature_columns:list[str] | None = None
+    ) -> None:
+        self.dataframe = dataframe
+        self.labels_names = sorted(labels_names)
+        self.feature_columns = feature_columns
+    
+    @property
+    def labels(self)-> pd.DataFrame | pd.Series:
+        return self.dataframe[self.labels_names]
+    
+    @property
+    def features(self)-> pd.DataFrame | pd.Series:
+        # if we do feature engineering, we may want to update self.dataframe after
+        # TrainSet initialization. 
+        # I'm not sure... Provisional pattern.
+        columns_set = set(self.dataframe.columns)
+        self.feature_columns = sorted(
+            list(columns_set.difference(self.labels_names))
+        )
+        return self.dataframe[self.labels_names]
 
-    # def __repr__(self) -> str:
-    #     return f"FoldData(feature_shape= {self.feature_train.shape}, \
-    #         label_shape= {self.label_test.shape})"
-
+class TestSet:
+    def __init__(self, 
+                 dataframe:pd.DataFrame,labels_names:list[str], 
+                 feature_columns:list[str] | None = None
+    ) -> None:
+        self.dataframe = dataframe
+        self.labels_names = sorted(labels_names)
+        self.feature_columns = feature_columns
+    
+    @property
+    def labels(self)-> pd.DataFrame | pd.Series:
+        return self.dataframe[self.labels_names]
+    
+    @property
+    def features(self)-> pd.DataFrame | pd.Series:
+        # if we do feature engineering, we may want to update self.dataframe after
+        # TrainSet initialization. 
+        # I'm not sure... Provisional pattern.
+        columns_set = set(self.dataframe.columns)
+        self.feature_columns = sorted(
+            list(columns_set.difference(self.labels_names))
+        )
+        return self.dataframe[self.labels_names]
 
 class ClassicalWindow(RollWindow):
     def __init__(
         self,
         dataframe:pd.DataFrame,
+        labels_names:list[str],
         window:int = 1,
         train_prop:float = 0.9,
         **kwargs
@@ -45,11 +84,20 @@ class ClassicalWindow(RollWindow):
         self.train_length = int(train_prop*dataframe.shape[0])
         self.dataframe = dataframe
         self.window = window
+        self.labels_names = labels_names
 
-    def create_folds(self) -> Generator[tuple[pd.DataFrame, pd.DataFrame],None,None]:
+    def create_folds(self) -> Generator[tuple[TrainSet, TestSet],None,None]:
         
         for index in range(self.train_length,self.dataframe.shape[0], self.window):
-            yield (self.dataframe.iloc[:index], self.dataframe.iloc[[index]])
+            yield (
+                TrainSet(
+                    self.dataframe.iloc[:index],self.labels_names
+                ),
+                TestSet(
+                    self.dataframe.iloc[[index]], self.labels_names
+                )
+                 
+            )
 
     # def __repr__(self) -> str:
     #     return f"FoldData(feature_shape= {self.feature_train.shape}, \
