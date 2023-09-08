afbe5fe0d9da0b505ae09be39be8ab6eef7fa4b4
I T <ivo.a.v.tavares@gmail.com>
2023-09-08 15:51:06 +0100

    Removed TrainSet, TestSet from rolling_windows_base.

--- preprocessing/roll_windows/roll_window_base.py
+++ preprocessing/roll_windows/roll_window_base.py
@@ -16,54 +16,6 @@ class RollWindow(ABC):
                  label_columns: list[str] | None = None):
         pass
 
-class TrainSet:
-    def __init__(self, 
-                 dataframe:pd.DataFrame,labels_names:list[str], 
-                 feature_columns:list[str] | None = None
-    ) -> None:
-        self.dataframe = dataframe
-        self.labels_names = sorted(labels_names)
-        self.feature_columns = feature_columns
-    
-    @property
-    def labels(self)-> pd.DataFrame | pd.Series:
-        return self.dataframe[self.labels_names]
-    
-    @property
-    def features(self)-> pd.DataFrame | pd.Series:
-        # if we do feature engineering, we may want to update self.dataframe after
-        # TrainSet initialization. 
-        # I'm not sure... Provisional pattern.
-        columns_set = set(self.dataframe.columns)
-        self.feature_columns = sorted(
-            list(columns_set.difference(self.labels_names))
-        )
-        return self.dataframe[self.labels_names]
-
-class TestSet:
-    def __init__(self, 
-                 dataframe:pd.DataFrame,labels_names:list[str], 
-                 feature_columns:list[str] | None = None
-    ) -> None:
-        self.dataframe = dataframe
-        self.labels_names = sorted(labels_names)
-        self.feature_columns = feature_columns
-    
-    @property
-    def labels(self)-> pd.DataFrame | pd.Series:
-        return self.dataframe[self.labels_names]
-    
-    @property
-    def features(self)-> pd.DataFrame | pd.Series:
-        # if we do feature engineering, we may want to update self.dataframe after
-        # TrainSet initialization. 
-        # I'm not sure... Provisional pattern.
-        columns_set = set(self.dataframe.columns)
-        self.feature_columns = sorted(
-            list(columns_set.difference(self.labels_names))
-        )
-        return self.dataframe[self.labels_names]
-
 class ClassicalWindow(RollWindow):
     def __init__(
         self,
@@ -86,17 +38,11 @@ class ClassicalWindow(RollWindow):
         self.window = window
         self.labels_names = labels_names
 
-    def create_folds(self) -> Generator[tuple[TrainSet, TestSet],None,None]:
+    def create_folds(self) -> Generator[tuple[pd.DataFrame, pd.DataFrame],None,None]:
         
         for index in range(self.train_length,self.dataframe.shape[0], self.window):
             yield (
-                TrainSet(
-                    self.dataframe.iloc[:index],self.labels_names
-                ),
-                TestSet(
-                    self.dataframe.iloc[[index]], self.labels_names
-                )
-                 
+                self.dataframe.iloc[:index],self.dataframe.iloc[[index]]
             )
 
     # def __repr__(self) -> str:
