4e5c7554bf2292a095037d6a031d2d7dcbf19b91
I T <ivo.a.v.tavares@gmail.com>
2023-09-09 21:53:38 +0100

    updated model_base file

--- models/model_base.py
+++ models/model_base.py
@@ -4,12 +4,13 @@ import numpy as np
 
 
 class ModelBase(Protocol):
+    # we cannot add the properties best_model, or custom_model, since for some models
+    # they are simple attributes, for others they are properties.
     def find_best(self,
         X_train:pd.DataFrame | pd.Series, y_train: pd.DataFrame | pd.Series,
         X_val:pd.DataFrame | pd.Series | None = None,
         y_val: pd.DataFrame | pd.Series | None = None,
-        # num_obs_val: int = 10, # This needs to be changed to a proportion
-        # show_progress_bar: bool = True
+        # we only need to include the input which has no default value.
     )-> None:...
     def fit_custom_model(self,
         X_df:pd.DataFrame | pd.Series, y_df:pd.DataFrame | pd.Series
