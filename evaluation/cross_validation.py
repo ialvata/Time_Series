1a8d778b31733e757e8ae3b39c09cc87132d7589
I T <ivo.a.v.tavares@gmail.com>
2023-09-09 22:08:15 +0100

    Added docstring to TuningOption class, and started evaluate method.

--- evaluation/cross_validation.py
+++ evaluation/cross_validation.py
@@ -4,27 +4,69 @@ from enum import Enum
 from preprocessing.feature_engineering.feature_engineering import FeatureEngineering
 
 class TuningOption(Enum):
-    HYPER_TUNING = 1
-    ONLY_TRAINING = 2
+    """
+    Represents the different possibilities when doing cross validation:
+
+        `NO_FITTING`:
+            In this case, we've already fitted a custom model, and defined the hyperparameters.
+            We only need to forecast the custom model with the test set.
+
+        `FIT_PARAM_ONLY`:
+            We fit the a custom model - with already defined hyperparameters - on the 
+            training set and and forecast it on the test set.
+        
+        `FIT_HYPER_PARAM`:
+            We use set the hyperparameters to different values, fit the custom model on the
+            train set, and evaluate the hyperparameters on the validation set. Then we forecast
+            on the test set.
+    """
+    FIT_HYPER_PARAM = 1
+    FIT_PARAM_ONLY = 2
     NO_FITTING = 3
 
 
 class CrossValidation:
     def __init__(self, models:list[ModelBase],metrics: list[Callable],
-                 data_generator:Generator, feat_eng_train_test = list[FeatureEngineering]
+                 data_generator:Generator, feat_eng = FeatureEngineering
                  ) -> None:
         self.models = models
         self.metrics = metrics
         self.data_generator = data_generator
-        self.feat_eng_train_test = feat_eng_train_test
+        self.feat_eng = feat_eng
+        self.forecasts = None
 
 
-    def _evaluate_only_training(self):...
+    def _evaluate_fit_param_only(self):
+        """
+        Method for the case when we use the whole training set to fit a model with 
+        hyperparameters already predefined
+        """
+        ...
 
     def _evaluate_no_fitting(self):...
 
-    def _evaluate_hyper_tuning(self):...
+    def _evaluate_fit_hyper_param(self):...
 
-    def evaluate(self):...
+    def evaluate(self, tuning_option: TuningOption):
+        for model in self.models:
+            for train_set,test_set  in self.data_generator:
+                #####################              Feature Engineering             ####################
+                self.feat_eng.set_datasets(train = train_set, test = test_set)
+                self.feat_eng.create_features(train_set)
+                self.feat_eng.create_features(test_set)
+                ####################               Evaluation type                #####################
+                match tuning_option:
+                    case TuningOption.FIT_HYPER_PARAM:
+                        ...
+                        model.forecast(X_test=self.feat_eng.features,use_best_model= True)
+                    case TuningOption.FIT_PARAM_ONLY:
+                        ...
+                        model.forecast(X_test=self.feat_eng.features, use_best_model= False)
+                    case TuningOption.NO_FITTING:
+                        ...
+                        model.forecast(X_test=self.feat_eng.features, use_best_model= False)
+                
+                        
+                        
 
     def show_results(self):...
