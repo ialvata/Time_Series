b877272f8087f3ba8c5cf3cf3700aae5844f529d
I T <ivo.a.v.tavares@gmail.com>
2023-09-11 18:23:07 +0100

    updated ModelBase: comments, docstring, and renaming some inputs.

--- models/model_base.py
+++ models/model_base.py
@@ -7,14 +7,20 @@ class ModelBase(Protocol):
     # we cannot add the properties best_model, or custom_model, since for some models
     # they are simple attributes, for others they are properties.
     def find_best(self,
-        X_train:pd.DataFrame | pd.Series, y_train: pd.DataFrame | pd.Series,
-        X_val:pd.DataFrame | pd.Series | None = None,
-        y_val: pd.DataFrame | pd.Series | None = None,
         # we only need to include the input which has no default value.
+        X_train:pd.DataFrame | pd.Series, y_train: pd.DataFrame | pd.Series,
+        # X_val:pd.DataFrame | pd.Series | None = None,
+        # y_val: pd.DataFrame | pd.Series | None = None,
+        
     )-> None:...
     def fit_custom_model(self,
-        X_df:pd.DataFrame | pd.Series, y_df:pd.DataFrame | pd.Series
-    )-> None:...
+        X_train:pd.DataFrame | pd.Series, y_train:pd.DataFrame | pd.Series,
+    )-> None:
+        """
+        This method should be able to also receive other keyword arguments, depending on the
+        model being wrapped. They should be gathered with `**kwargs` trick.
+        """
+        ...
     def forecast(self,
         X_test:pd.DataFrame | pd.Series, use_best_model:bool = False
     )-> np.ndarray:...
