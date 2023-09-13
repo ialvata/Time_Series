8128eb1be636db3f6053bd05bb1d11fb3200a06a
I T <ivo.a.v.tavares@gmail.com>
2023-09-13 19:14:45 +0100

    updated cross_validation. Seems to work.

--- evaluation/cross_validation.py
+++ evaluation/cross_validation.py
@@ -27,12 +27,21 @@ class TuningOption(Enum):
     FIT_PARAM_ONLY = 2
     NO_FITTING = 3
 
-class TrialResults:
-    def __init__(self, 
-        forecast:float|pd.DataFrame|pd.Series|np.ndarray, hyperparameters:dict | None = None
+class TrialHistory:
+    def __init__(self,
+        trial:int,
+        model_name:str,
+        forecast:np.ndarray, 
+        observation: np.ndarray,
+        hyperparameters:dict | None = None,
     ) -> None:
         self.hyperparameters = hyperparameters
         self.forecast = forecast
+        self.observation = observation
+        self.trial = trial
+        self.model_name = model_name
+    def __repr__(self) -> str:
+        return f"TrialHistory(trial={self.trial}, model={self.model_name})"
 
 class CrossValidation:
     def __init__(self, models:list[ModelBase],metrics: list[MetricBase],
@@ -43,19 +52,21 @@ class CrossValidation:
         self.data_generator = data_generator
         self.feat_eng = feat_eng
         self.forecasts = None
-        self.history = []
         self.observations = []
+        self.histories = {f"{model.name}":[] for model in models}
 
     def evaluate(self, tuning_option: TuningOption):
-        for model in self.models:
-            for train_set,test_set  in self.data_generator:
-                #####################              Feature Engineering             ####################
-                self.feat_eng.set_datasets(train_set = train_set, test_set = test_set)
-                # outside of cross validation
-                # in the main code self.feat_eng.add_pipeline()
-                self.feat_eng.create_features(destiny_set = "train")
-                self.feat_eng.create_features(destiny_set = "test")
-                ####################               Evaluation type                #####################
+        for trial,(train_set,test_set)  in enumerate(self.data_generator):
+            print(len(train_set))
+            ###################              Feature Engineering              #################
+            self.feat_eng.set_datasets(train_set = train_set, test_set = test_set)
+            # outside of cross validation
+            # in the main code self.feat_eng.add_pipeline()
+            self.feat_eng.create_features(destiny_set = "train")
+            self.feat_eng.create_features(destiny_set = "test")
+            ##################               Evaluation type              #####################
+            for model in self.models:
+                history = self.histories[f"{model.name}"]
                 match tuning_option:
                     case TuningOption.FIT_HYPER_PARAM:
                         model.find_best(
@@ -66,13 +77,15 @@ class CrossValidation:
                         forecast = model.forecast(
                             X_test=self.feat_eng.test_set.features,use_best_model= True
                         )
-                        self.history.append(
-                            TrialResults(
+                        history.append(
+                            TrialHistory(
+                                trial=trial,
+                                model_name= model.name,
                                 hyperparameters=model.best_hyperparameters,
-                                forecast=forecast                                
+                                forecast=np.array(forecast),
+                                observation= np.array(self.feat_eng.test_set.labels)                            
                             )
-                        )
-                        
+                        )  
                     case TuningOption.FIT_PARAM_ONLY:
                         model.fit_custom_model(
                             X_train=self.feat_eng.train_set.features,
@@ -81,28 +94,58 @@ class CrossValidation:
                         forecast = model.forecast(
                             X_test=self.feat_eng.test_set.features, use_best_model= False
                         )
-                        self.history.append(
-                            TrialResults(forecast=forecast)
+                        history.append(
+                            TrialHistory(
+                                trial=trial, model_name= model.name,
+                                forecast=np.array(forecast),
+                                observation= np.array(self.feat_eng.test_set.labels)
+                            )
                         )
                     case TuningOption.NO_FITTING:
                         forecast = model.forecast(
                             X_test=self.feat_eng.test_set.features, use_best_model= False
                         )
-                        self.history.append(
-                            TrialResults(forecast=forecast)
+                        history.append(
+                            TrialHistory(
+                                trial = trial, model_name= model.name,
+                                forecast = np.array(forecast),
+                                observation = np.array(self.feat_eng.test_set.labels)
+                            )
                         )
-        self.observations = self.feat_eng.test_set.labels
+                self.histories[f"{model.name}"]= history
 
-    def show_results(self) -> pd.DataFrame:
-        forecasts = np.ndarray([trial_res.forecast for trial_res in self.history])
-        metrics = {
-            f"{metric.name}": metric.compute(
-                predictions = forecasts, observations = np.array(self.observations)
-            )
-            for metric in self.metrics
+
+    def show_results(self, model_names:str | list[str] |None = None) -> pd.DataFrame:
+        """
+        TODO:
+            `model_names:str|list[str]|None = None` 
+                We should implement for the case of list[str]-
+        """
+        if model_names is None:
+            model_names = [model.name for model in self.models]
+        if isinstance(model_names,str):
+            model_names = [model_names]
+        if model_names is None:
+            raise Exception("List of models associated to this CrossValidation is empty!")
+        
+        forecasts = {
+            f"{model_name}": np.ravel(
+                [trial.forecast for trial in self.histories[model_name]]
+            ) for model_name in model_names
         }
-        table_res = pd.DataFrame(metrics)
+        observations = {
+            f"{model_name}": np.ravel(
+                [trial.observation for trial in self.histories[model_name]]
+            ) for model_name in model_names
+        }
+        metrics = {f"{metric.name}": [
+            metric.compute(
+                predictions = forecasts[model_name], observations = observations[model_name]
+            ) for model_name in model_names]for metric in self.metrics
+        }
+
+        table_res = pd.DataFrame(metrics, index=model_names)
         table_res.style.format(
-            '{:.4f}'
+                '{:.4f}'
         ).highlight_min(color='green')
         return table_res
